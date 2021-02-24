5b8d86ecb74d22bd2852eb4d37fc7d91355edd6c
francisco ximenes <fximenessilva@gmail.com>
2021-02-24 01:00:24 +0100

    new styles halfway

--- public/js/main.js
+++ public/js/main.js
@@ -10,7 +10,7 @@ const chatForm = document.getElementById('chat-form');
 const chatMessages = document.querySelector('.chat-messages');
 const roomName = document.getElementById('room-name');
 const userList = document.getElementById('users');
-const modalList = document.getElementById('user-modal');
+// const modalList = document.getElementById('user-modal');
 
 // Get username and room from URL
 const {
@@ -79,26 +79,27 @@ function outputMessage(message) {
 
 // Add room name to DOM
 function outputRoomName(room) {
-  roomName.innerText = room;
+  roomName.innerText = `${room} Room`;
 }
 
 // Add users to DOM
 function outputUsers(users) {
   userList.innerHTML = '';
-  modalList.innerHTML = '';
+  // modalList.innerHTML = '';
+  console.log(users);
 
   users.forEach((user) => {
     const li = document.createElement('li');
 
-    li.className = ('usr btn btn-primary');
-    li.setAttribute('type', 'button');
-    li.setAttribute('data-bs-toggle', 'modal');
-    li.setAttribute('data-bs-target', '#exampleModal');
+    li.className = ('usr btn btn-primary userFromList');
+    // li.setAttribute('type', 'button');
+    // li.setAttribute('data-bs-toggle', 'modal');
+    // li.setAttribute('data-bs-target', '#exampleModal');
     li.innerText = user.username;
     userList.appendChild(li);
 
-    const lii = document.createElement('li');
-    lii.innerText = user.email;
-    modalList.appendChild(lii);
+    // const lii = document.createElement('li');
+    // lii.innerText = user.email;
+    // modalList.appendChild(lii);
   });
 }
